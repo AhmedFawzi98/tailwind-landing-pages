b533ebe83c46b3397bb91cb3214b0cfbe5117adc
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-04-21 20:23:46 +0200

    Implement Hero, Snippets, Features, Access sections

--- clipboard/tailwind.config.js
+++ clipboard/tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
     content: ["./*.html"],
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ["Bai Jamjuree", "ui-sans-serif", "sans-serif"],
+        },
+        extend: {
+            colors: {
+                strongCyan: "hsl(171, 66%, 44%)",
+                lightBlue: "hsl(233, 100%, 69%)",
+                darkGrayishBlue: "hsl(210, 10%, 33%)",
+                grayishBlue: "hsl(201, 11%, 66%)",
+            },
+        },
     },
     plugins: [],
 };
