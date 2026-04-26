4e6e0595b7ec1eba01ae591efcca6a1047efdaeb
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-04-26 12:10:35 +0300

    Add Loopstudios landing page setup

---
+++ loopstudios/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    content: ["./*.html"],
+    theme: {
+        fontFamily: {
+            sans: ["Bai Jamjuree", "ui-sans-serif", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
