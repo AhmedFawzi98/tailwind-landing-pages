76ad00ff0d94f1913d2260da427c1fa181ab8286
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-04-27 21:50:01 +0300

    Shortly: add setup

---
+++ shortly/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: ["./*.html"],
+    theme: {
+        fontFamily: {
+            sans: ["Josefin Sans", "ui-sans-serif", "sans-serif"],
+            alata: ["Alata", "ui-sans-serif", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
