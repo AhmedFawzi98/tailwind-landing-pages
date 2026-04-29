5d2b4d98660f0dd3802fa16c318feb0476b5e8dd
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-04-29 22:52:45 +0300

    Shortly: Implement CTA and footer sections

--- shortly/tailwind.config.js
+++ shortly/tailwind.config.js
@@ -8,10 +8,12 @@ module.exports = {
                 cyanDark: "hsl(180, 66%, 35%)",
                 darkViolet: "hsl(257, 27%, 26%)",
                 red: "hsl(0, 87%, 67%)",
-                grayishViolet: "hsl(257, 7%, 69%)",
+                grayishViolet: "hsl(257, 7%, 59%)",
+                grayishVioletDark: "hsl(257, 7%, 39%)",
                 grayishVioletLight: "hsl(257, 7%, 73%)",
                 veryDarkBlue: "hsl(255, 11%, 22%)",
                 veryDarkViolet: "hsl(260, 8%, 14%) ",
+                whiteViolet: "hsl(257, 10%,85%)",
             },
             fontFamily: {
                 sans: ["Poppins", "sans-serif"],
