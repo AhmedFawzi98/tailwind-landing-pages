69f2bd67ccaccf2da2241e66aa64f39ecc6973eb
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-05-01 19:56:42 +0300

    Fylo: Implement cta section

--- fylo/tailwind.config.js
+++ fylo/tailwind.config.js
@@ -9,7 +9,9 @@ export default {
                 darkBlue2: "hsl(216, 53%, 9%)",
                 darkBlue3: "hsl(219, 30%, 18%)",
                 accentCyan: "hsl(176, 68%, 64%)",
+                accentCyan2: "hsl(190,68%, 24%)",
                 accentBlue: "hsl(198, 60%, 50%)",
+                accentBlue2: "hsl(210, 60%, 40%)",
                 lightRed: "hsl(0, 100%, 63%)",
             },
             fontFamily: {
