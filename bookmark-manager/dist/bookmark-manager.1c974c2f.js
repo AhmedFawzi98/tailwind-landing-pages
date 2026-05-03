beba393f7c0d9ed6f2fc97ede6edf642fef34625
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-05-03 13:15:03 +0300

    Bookmark-Manager: Implement header and enable hamburger menu

--- bookmark-manager/dist/bookmark-manager.1c974c2f.js
+++ bookmark-manager/dist/bookmark-manager.1c974c2f.js
@@ -1,18 +1,17 @@
 "use strict";
-
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const mainNav = document.querySelector("#main-nav");
 const mainNavList = document.querySelector(".main-nav-list");
-
 //toggle hamburger menu
-hamburgerBtn.addEventListener("click", () => {
+hamburgerBtn.addEventListener("click", ()=>{
     const isExpanded = mainNav.classList.toggle("nav-open");
     hamburgerBtn.setAttribute("aria-expanded", isExpanded ? "true" : "false");
 });
-
 //close hamburger menu when link is clicked
-mainNavList.addEventListener("click", (event) => {
+mainNavList.addEventListener("click", (event)=>{
     if (event.target.tagName !== "A") return;
     mainNav.classList.remove("nav-open");
     hamburgerBtn.setAttribute("aria-expanded", "false");
 });
+
+//# sourceMappingURL=bookmark-manager.1c974c2f.js.map
--- bookmark-manager/tailwind.config.js
+++ bookmark-manager/tailwind.config.js
@@ -4,7 +4,10 @@ export default {
         extend: {
             colors: {
                 softBlue: "hsl(231, 69%, 60%)",
+                softBlueShade1: "hsl(231, 69%, 40%)",
+                softBlueShade2: "hsl(231, 60%, 20%)",
                 softRed: "hsl(0, 94%, 66%)",
+                softRedShade1: "hsl(0, 94%, 50%)",
                 grayishBlue: "hsl(229, 8%, 60%)",
                 veryDarkBlue: "hsl(229, 31%, 21%)",
             },
