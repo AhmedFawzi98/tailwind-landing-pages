9fed62568ef63a62de21803a0ec47f504da87010
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-05-05 23:55:42 +0300

    implement testimonials grid with text clamp and see more / see less buttons

--- testimonials/js/script.js
+++ testimonials/js/script.js
@@ -1,1 +1,40 @@
-"use strict";
+document.querySelectorAll(".testimonial-card").forEach((card) => {
+    const text = card.querySelector(".testimonial-quote-secondary");
+    const btn = card.querySelector(".clamp-trigger-btn");
+
+    if (!text || !btn) return;
+
+    let expanded = false;
+
+    function updateClampTriggerBtnState() {
+        if (expanded) {
+            btn.classList.remove("hidden");
+            btn.textContent = "See less";
+        } else if (text.scrollHeight > text.clientHeight) {
+            btn.classList.remove("hidden");
+            btn.textContent = "See more";
+        } else {
+            btn.classList.add("hidden");
+        }
+    }
+
+    // capture all clamp-related classes of that text .. dynamically
+    const clampClasses = [...text.classList].filter((c) =>
+        c.includes("line-clamp"),
+    );
+
+    btn.addEventListener("click", () => {
+        expanded = !expanded;
+
+        if (expanded) {
+            text.classList.remove(...clampClasses);
+        } else {
+            text.classList.add(...clampClasses);
+        }
+
+        updateClampTriggerBtnState(); // re-evaluate after toggle
+    });
+
+    window.addEventListener("load", updateClampTriggerBtnState);
+    window.addEventListener("resize", updateClampTriggerBtnState);
+});
--- testimonials/tailwind.config.js
+++ testimonials/tailwind.config.js
@@ -1,27 +1,14 @@
-module.exports = {
+export default {
     content: ["./*.html"],
     theme: {
         extend: {
-            colors: {
-                cyan: "hsl(180, 66%, 49%)",
-                cyanLight: "hsl(180, 66%, 69%)",
-                cyanDark: "hsl(180, 66%, 35%)",
-                darkViolet: "hsl(257, 27%, 26%)",
-                red: "hsl(0, 87%, 67%)",
-                grayishViolet: "hsl(257, 7%, 59%)",
-                grayishVioletDark: "hsl(257, 7%, 39%)",
-                grayishVioletLight: "hsl(257, 7%, 73%)",
-                veryDarkBlue: "hsl(255, 11%, 22%)",
-                veryDarkViolet: "hsl(260, 8%, 14%) ",
-                whiteViolet: "hsl(257, 10%,85%)",
-            },
             fontFamily: {
-                sans: ["Poppins", "sans-serif"],
+                sans: ["Barlow Semi Condensed", "sans-serif"],
             },
             spacing: {
                 180: "32rem",
             },
         },
     },
-    plugins: [],
+    plugins: ["@tailwindcss/line-clamp"],
 };
